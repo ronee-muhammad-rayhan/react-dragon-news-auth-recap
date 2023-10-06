3df0df2e6c77c08d7a0bc275f99d651f9f73745a
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-06 15:25:05 +0600

    52_5-5 Load News and create News detail page

---
+++ src/pages/Home/NewsCard.jsx
@@ -0,0 +1,25 @@
+
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom";
+const NewsCard = ({ news }) => {
+    const { title, image_url, details, _id } = news;
+    return (
+        <div className="card bg-base-100 shadow-xl mb-16 border-b-2">
+            <figure><img src={image_url} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                {
+                    details.length > 200
+                        ? <p>{details.slice(0, 200)} <Link className="text-blue-600 font-bold" to={`/news/${_id}`}>Read More...</Link></p>
+                        : <p>{details}</p>
+                }
+            </div>
+        </div>
+    );
+};
+
+NewsCard.propTypes = {
+    news: PropTypes.any
+}
+
+export default NewsCard;
