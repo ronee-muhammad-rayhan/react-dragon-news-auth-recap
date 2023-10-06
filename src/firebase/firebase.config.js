7e86e64450b85ab1fe31936a23322d529b3eceef
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-06 13:28:48 +0600

    52_5-3 (Recap) Create Auth Context and integrate Register

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5t8sldRyoEzuicQC5GxlqUtopigFDrWY",
-  authDomain: "react-dragon-news-auth.firebaseapp.com",
-  projectId: "react-dragon-news-auth",
-  storageBucket: "react-dragon-news-auth.appspot.com",
-  messagingSenderId: "750044483429",
-  appId: "1:750044483429:web:77f4f5d3f33234b21f5b8c"
+  apiKey: "AIzaSyBXkDR7UUR0PlnHc_YdGABKUV76cOUgajc",
+  authDomain: "react-dragon-news-auth-4940a.firebaseapp.com",
+  projectId: "react-dragon-news-auth-4940a",
+  storageBucket: "react-dragon-news-auth-4940a.appspot.com",
+  messagingSenderId: "656727064016",
+  appId: "1:656727064016:web:b375d82456490787430dd5",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
+export const app = initializeApp(firebaseConfig);
